7f2cab78fcdccab3579355ccb694dfc822eedd9e
Anton <antosha931@rambler.ru>
2022-01-22 00:06:58 +0400

    technical task completed and animation has been additionally added

--- VK Project/Cell/FotoCollectionViewCell.swift
+++ VK Project/Cell/FotoCollectionViewCell.swift
@@ -11,16 +11,28 @@ class FotoCollectionViewCell: UICollectionViewCell {
     
     @IBOutlet weak var backView: UIView!
     @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var likeButton: UIButton!
+    @IBOutlet weak var likeImage: UIImageView!
+    @IBOutlet weak var numberLikeLabel: UILabel!
     
     var savedObject: Any?
     
-    func clearCell() {
-        photoImageView.image = nil
-        savedObject = nil
+    var numberLikes: Int = 0
+    
+    var statusLike = false
+    
+    func setup() {
+        likeImage.image = UIImage(systemName: "heart")
+        likeImage.tintColor = UIColor.red
+        numberLikeLabel.textColor = UIColor.black
+        numberLikeLabel.textAlignment = .center
+        numberLikeLabel.font = UIFont(name: "System", size: 20.0)
     }
     
-    func configure(image: UIImage) {
-        photoImageView.image = image
+    func clearCell() {
+        savedObject = nil
+        photoImageView.image = nil
+        numberLikeLabel.text = nil
     }
     
     override func prepareForReuse() {
@@ -28,9 +40,50 @@ class FotoCollectionViewCell: UICollectionViewCell {
         clearCell()
     }
     
+    func configure(image: UIImage) {
+        photoImageView.image = image
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
+        setup()
         clearCell()
     }
-
+    
+    func animateLikeButton() {
+        UIView.transition(with: likeImage,
+                          duration: 1,
+                          options: .transitionFlipFromLeft,
+                          animations: {
+                            self.likeImage.image = UIImage(systemName: "heart.fill")
+                          },
+                          completion: {_ in
+                            self.numberLikes += 1
+                            self.numberLikeLabel.text = String(self.numberLikes)
+                            self.numberLikeLabel.isHidden = false
+                            self.statusLike = true
+                          })
+    }
+    
+    func animateCancelLikeButton() {
+        UIView.transition(with: likeImage,
+                          duration: 1,
+                          options: .transitionCrossDissolve,
+                          animations: {
+                            self.likeImage.image = UIImage(systemName: "heart")
+                          },
+                          completion: {_ in
+                            self.numberLikeLabel.isHidden = true
+                            self.numberLikes -= 1
+                            self.statusLike = false
+                          })
+    }
+    
+    @IBAction func likeButton(_ sender: Any) {
+        if statusLike == false {
+            animateLikeButton()
+        } else {
+            animateCancelLikeButton()
+        }
+    }
 }
